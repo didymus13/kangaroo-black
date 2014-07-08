36efc141726225a1809146a9fa33f751df980b9c
Stephane Doiron <didymus1313@gmail.com>
2014-07-07 20:43:30 -0400

    profile template mock created

--- campaignManager/campaignManager/profiles/models.py
+++ campaignManager/campaignManager/profiles/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from social.apps.django_app.default.models import UserSocialAuth
 #from campaignManager.armies.models import Game
 from django_countries.fields import CountryField
+from django.forms import ModelForm
 
 class Profile(models.Model):
     user = models.OneToOneField(UserSocialAuth)
@@ -10,6 +11,12 @@ class Profile(models.Model):
     location = models.CharField(max_length=124, blank=True, null=True)
     country = CountryField(null=True, blank=True)
     games = models.ManyToManyField('armies.Game')
+    bio = models.TextField(blank=True)
     
     def __unicode__(self):
         return self.username
+
+class ProfileForm(ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['username', 'email', 'location', 'country']
--- campaignManager/campaignManager/profiles/views.py
+++ campaignManager/campaignManager/profiles/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render, render_to_response
+from django.shortcuts import render, render_to_response, get_object_or_404
 from django.template.context import RequestContext
+from campaignManager.profiles.models import *
+from django.contrib.auth.decorators import login_required
 
 def home(request):
    context = RequestContext(request,
@@ -7,3 +9,22 @@ def home(request):
                             'user': request.user})
    return render_to_response('home.html',
                              context_instance=context)
+
+@login_required                             
+def edit(request):
+    profile = Profile.objects.get(user=request.user.id);
+    form = ProfileForm(instance=profile)
+    return render_to_response('form.html', {
+        'request': request,
+        'user': request.user,
+        'form': form
+    })
+    
+def detail(request, pk):
+    profile = get_object_or_404(Profile, pk=pk)
+    print profile;
+    return render_to_response('detail.html', {
+        'request': request,
+        'profile': profile
+    })
+    
