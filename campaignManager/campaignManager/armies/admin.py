ba0f6cec7fbe42f2dc57535ef4cf50c1ca01370d
Stephane Doiron <didymus1313@gmail.com>
2014-07-13 20:44:06 -0400

    Easier to enter factions now. Still would need a front-facing CRUD with moderation?

--- campaignManager/campaignManager/armies/admin.py
+++ campaignManager/campaignManager/armies/admin.py
@@ -2,6 +2,19 @@ from django.contrib import admin
 from models import *
 
 # Register your models here.
+class CommonAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ("name", )}
+
 admin.site.register(Army)
-admin.site.register(Game)
-admin.site.register(Faction)
+
+class FactionAdmin(CommonAdmin):
+    pass
+admin.site.register(Faction, FactionAdmin)
+
+class FactionTabular(admin.TabularInline):
+    model = Faction
+    
+class GameAdmin(CommonAdmin):
+    inlines = [FactionTabular,]
+    
+admin.site.register(Game, GameAdmin)
